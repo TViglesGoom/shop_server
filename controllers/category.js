8bf7f36b05030c467fe70df0a27e9239aaf00146
TViglesGoom <tviglesgoom@gmail.com>
2020-10-22 22:40:52 +0300

    feat: added category controller

---
+++ controllers/category.js
@@ -0,0 +1,44 @@
+const {CategoryCRUD} = require('../db');
+
+module.exports = {
+    async addCategory(req, res) {
+        try {
+            const Category = await CategoryCRUD.addCategory(req.body);
+            res.status(200).send(Category);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    },
+    async listAllCategories(req, res) {
+        try {
+            const Categories = await CategoryCRUD.listAllCategories();
+            res.status(200).send(Categories);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    },
+    async findCategoryById(req, res) {
+        const {id} = req.params;
+        try {
+            const Category = await CategoryCRUD.findCategory(id);
+            res.status(200).send(Category);
+        } catch (err) {
+            res.status(404).send({message: `Category with id ${id} was not found`});
+        }
+    },
+    async removeCategory(req, res) {
+        const {id} = req.params;
+        let Category;
+        try {
+            Category = await CategoryCRUD.findCategory(id);
+        } catch (err) {
+            return res.status(404).send({message: `Category with id ${id} was not found`});
+        }
+        try {
+            Category = await CategoryCRUD.removeCategory(Category);
+            res.status(204).send(Category);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    },
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const ProductController = require('./product');
 const SupplierController = require('./supplier');
+const CategoryController = require('./category');
 
 module.exports = {
     ProductController,
     SupplierController,
+    CategoryController,
 };
